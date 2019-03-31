ccfc19eabf8a64da6ca36424a4bc990e800cb827
Joseph <jarm061198@gmail.com>
2019-03-31 01:59:01 -0600

    nuevos cambio

--- database/migrations/2019_03_31_075325_create_cash_report.php
+++ database/migrations/2019_03_31_075325_create_cash_report.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CashReport extends Migration
+class CreateCashReportsTable extends Migration
 {
     /**
      * Run the migrations.
