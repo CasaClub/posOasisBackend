195f1ed64779fd73717d6fa5b8433e88ff56cf48
Joseph <jarm061198@gmail.com>
2019-03-31 02:02:52 -0600

    Todo por no devolver el commit

--- database/migrations/2019_03_31_080044_create_cash_reports_table.php
+++ database/migrations/2019_03_31_080044_create_cash_reports_table.php
@@ -15,6 +15,12 @@ class CreateCashReportsTable extends Migration
     {
         Schema::create('cash_reports', function (Blueprint $table) {
             $table->increments('id');
+            $table->timestamp('start_report');
+            $table->timestamp('end_report')->nullable();
+            $table->unsignedInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->decimal('effective');
+            $table->decimal('dataphone');
             $table->timestamps();
         });
     }
